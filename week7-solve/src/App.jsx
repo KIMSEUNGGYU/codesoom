b3222ec3c870306007893d4fd7d3dec12b7516c5
KIMSEUNGGYU <dev.seunggyu@gmail.com>
2022-01-15 13:49:41 +0900

    [solve] 7주차 해설 - 레스토랑 리뷰 남기기 - 초기 설정

---
+++ week7-solve/src/App.jsx
@@ -0,0 +1,59 @@
+import React from 'react';
+
+import {
+  Switch,
+  Route,
+  Link,
+} from 'react-router-dom';
+
+import { useDispatch } from 'react-redux';
+
+import RestaurantsPage from './RestaurantsPage';
+import RestaurantPage from './RestaurantPage';
+import HomePage from './HomePage';
+import AboutPage from './AboutPage';
+import LoginPage from './LoginPage';
+import NotFoundPage from './NotFoundPage';
+
+import { setAccessToken } from './actions';
+
+import { loadItem } from './services/storage';
+
+export default function App() {
+  // const { location: { pathname } } = window;
+
+  // const MyComponent = {
+  //   '/': HomePage,
+  //   '/about': AboutPage,
+  //   '/restaurants': RestaurantsPage,
+  // }[pathname] || NotFoundPage;
+
+  const dispatch = useDispatch();
+
+  // TODO: localStorage 에서 accessToken 가져오기
+  const accessToken = loadItem('accessToken');
+  if (accessToken) {
+    dispatch(setAccessToken(accessToken));
+  }
+
+  return (
+    <div>
+      <header>
+        <h1>
+          <Link to="/">
+            헤더 영역
+          </Link>
+        </h1>
+      </header>
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/about" component={AboutPage} />
+        <Route path="/login" component={LoginPage} />
+        <Route exact path="/restaurants" component={RestaurantsPage} />
+        <Route path="/restaurants/:id" component={RestaurantPage} />
+        <Route component={NotFoundPage} />
+      </Switch>
+    </div>
+
+  );
+}
