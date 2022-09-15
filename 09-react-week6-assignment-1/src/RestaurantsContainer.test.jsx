b263754ca6657b24e3cb7b39c016e0293a27b459
ㄱㅅㄱ <45627868+KIMSEUNGGYU@users.noreply.github.com>
2022-09-15 21:32:31 +0900

    [코드숨] 리액트 과정 6주 1 (#9)
    * First commit
    
    * Fix audit
    
    * Upgrade dependencies
    
    Upgrade React to 17.
    Upgrade Webpack to v5.
    
    * [chore] react-router-dom 패키지 설치
    
    * [test] Header 렌더 테스트 추가
    
    * [feat] 헤더 컴포넌트 기능 구현
    
    * [test] Home page 렌더 테스트 추가
    
    * [feat] Home page 기능 구현
    
    * [test] App 테스트 기본 렌더링 하도록 수정 (임시)
    
    * [test] HomePage About 클릭시 이동 테스트 코드 추가
    
    * [refactor] 이동 url 수정
    
    * [test] HomePage 렌더 테스트 수정
    
    * [test] About page 테스트 추가
    
    * About 페이지 렌더 기능 구현
    
    * [test] RestaurantsPage 테스트 추가
    
    * [feat] RestautansPage 기능 구현
    
    * [refactor] HomePage 에서 Header 컴포넌트 제거
    
    * [test] api fetchRestaurant api test 작성
    
    * [feat] fetchRestaurant api 기능 구현
    
    * [test] restaurants 라우터 적용으로 인한 테스트 코드 수정
    
    * [refactor] restaurant 클릭 시 화면 전환(라우팅) 기능 구현
    
    * [test] reducer setRestaurant 테스트 구현
    
    * [feat] reducer setRestaurant 기능 구현
    
    * [test] redux thunk loadRestaurant 테스트 구현
    
    * [chore] given2 패키지 설치 및 설정
    
    * [refactor] reducer 초기값 변경
    
    * [test] restaurant 컴포넌트 렌더 테스트 구현
    
    * [feat] 레스토랑 컴포넌트 렌더 기능 구현
    
    * [test] Restaurant 페이지 렌더 테스트
    
    * [feat] restaurant 페이지 렌더 기능 구현
    
    * [refactor] Restaurant 수정
    
    * [test] Restaurant Page 수정
    
    * [feat] 레스토랑 페이지 url 의 id 에 따라 동적으로 변경 기능 구현
    
    * [refactor] actions loadRestaurt 인자 전달 수정
    
    * [test] NotFoundPage 렌더 테스트
    
    * [feat] NotFoundPage 렌더 기능 구현
    
    * [test] App test path 별 page 렌더 테스트
    
    * [feat] App 컴포너트 path 별 page 렌더 기능 구현
    
    * [env] webpack 설정
    
    * [feat] react-route 설정
    
    * [refactor] 피드백 수용
    - render 시 (()) 로 씌우기
    - 데이터 없을 시 렌더 메시지 적용
    - h1 다음에 h2 적용
    
    * [test] reducer resetRestaurant 테스트 구현
    
    * [feat] resetRestaurant 기능 구현
    
    * [feat] 페이지 이탈시, restaurant 초기화 기능 구현
    
    * [refactor] restaurantPage selector 가져오는 방식 변경
    
    Co-authored-by: hannut91 <hannut91@gmail.com>

---
+++ 09-react-week6-assignment-1/src/RestaurantsContainer.test.jsx
@@ -0,0 +1,24 @@
+import { render } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
+
+import { useSelector } from 'react-redux';
+
+import RestaurantsContainer from './RestaurantsContainer';
+
+test('RestaurantsContainer', () => {
+  useSelector.mockImplementation((selector) => selector({
+    restaurants: [
+      { id: 1, name: '마법사주방' },
+    ],
+  }));
+
+  const { container } = render((
+    <MemoryRouter>
+      <RestaurantsContainer />
+    </MemoryRouter>
+  ));
+
+  expect(container).toHaveTextContent('마법사주방');
+});
+
+// TODO: 레스로탕 name 클릭시 화면 전환 테스트
