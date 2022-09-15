54242976361a0bfa2488f84661519cfc599fb92c
ㄱㅅㄱ <45627868+KIMSEUNGGYU@users.noreply.github.com>
2022-09-15 21:31:46 +0900

    [코드숨] 리액트 과정 5주 1 (#8)
    * First commit
    
    * Fix type
    
    * Fix audit
    
    * Upgrade dependencies
    
    Upgrade React to 17.
    Upgrade Webpack to v5.
    
    * [chore] 프로젝트 초기 설정
    
    * [test] Categories 컴포넌트 렌더 테스트 추가
    
    * [faet] category render 기능 구현
    
    * [test] Categories 테스트 코드 작성
    - react-reudx mocking
    - categories 테스트 값 선언
    
    * [feat] CategoriContainer 기능 구현
    - 액션 기능 정의
    - 로딩시 category 요청 기능 구현
    
    * [test] CategoriesContainer 테스트 코드 삭제
    - 로딩시 카테고리 요청하는 것은 App 에서 처리로 인한 테스트 코드 제거
    
    * [refactor] 로딩시 categori 설정은 App 처리로 인한 코드 변경
    
    * [test] reducer setCategories 테스트 코드 추가
    
    * [feat] reducer setCategories 기능 구현
    
    * [refactor] fixture 네임 변경 => fixtures
    
    * [refactir] CategoriesContainer UI 변경
    
    * [test] CategoriesContainer 테스트 사용하지 않는 코드 제거
    
    * [test] App 테스트 코드 추가
    - 로드시, 카테고리 상태 변경
    
    * [test] App 로드시 categories 상태 변경 테스트 추가
    
    * [refactor] 테스트 이름 변경, 불필요한 코드 제거 및 테스트 코드 수정
    
    * [test] RegionContainer 테스트 코드 작성
    - regions 렌더링 테스트
    
    * [feat] Region 컨테이너 렌더 기능 구현
    
    * [test] Regions 컴포넌트 렌더 테스트 추가
    
    * [feat] Regions 컴포넌트 렌더 기능 구현
    
    * [refactor] Regions 컨테이너 Regions 컴포넌트 렌더하도록 수정
    
    * [refactor] Categories 컨테이너 Categories 컴포넌트 렌더하도록 수정
    
    * [test] regions fixtures 데이터 추가
    
    * [test] App 로드시, regions / categories 초기화 테스트 추가
    
    * [feat] regions 요청 기능 구현
    
    * [test] App 컴포넌트 렌더 테스트 추가
    
    * [feat] App 컴포넌트 지역, 카테고리 렌더 기능 구현
    
    * [test] reducer setRegions 테스트 추가
    
    * [feat] reducer setRegions 기능 구현
    
    * [test] expect 처리 안 한 부분 처리
    
    * [test] Item 컴포넌트 렌더 테스트 추가
    
    * [feat] Item 컴포넌트 렌더 기능 구현
    
    * [refactor] Item 컴포넌트 렌더 하도록 수정
    
    * [test] Item 컴포넌트 버튼 클릭시 테스트 추가
    
    * [test] reducer setSelectedRegion 테스트 추가
    
    * [feat] reducer setSelected 기능 구현
    
    * [test] Item 클릭시 dispatch 요청 테스트 추가
    
    * [test] Item 컴포넌트 테스트 추가
    
    * [refactor] Item 컴포넌트 구조 변경
    
    * [refactor] 카테고리 컴포넌트 구조 변경
    
    * [test] 카테고리 컨테이너 테스트 코드 변경
    
    * [refactor] 카테고리 컨테이너 구조 변경
    
    * [test] RegionContainer 테스트 수정
    
    * [refactor] Region 컨테이너, 컴포넌트 구조 변경
    
    * [test] reducer set selectedCategory 테스트 코드 추가
    
    * [feat] reducer setSelectedCategory 기능 구현
    
    * [refactor] 전체 구조 변경
    
    * [test] Restaurants 컴포넌트 테스트 추가
    
    * [feat] Restaurants 컴포넌트 기능 구현
    
    * [test] Restaurants 컨테이너 테스트 추가
    
    * [test] reducer set restaurants 테스트 추가
    
    * [feat] reducer setRestaurants 기능 구현
    
    * [test] App 컴포넌트 restaurants 렌더 기능 구현
    
    * [refactor] setRestaurants api 연동
    
    * [feat] redux 연동
    
    * [chore] test unit 스크립트 변경
    
    * [test] regions categories 클릭된 버튼이  있는 경우 테스트 추가
    
    * [test] reducer function test 추가
    
    * [test] api test 작성
    
    * [chore] redux-mock-store 패키지 설치
    
    * [test] actions - thunk 테스트 추가
    
    * [refactor] 불 필요한 코드 제거
    
    * [chore] given2 pacakge 설정
    
    * [test] given 추가
    
    Co-authored-by: hannut91 <hannut91@gmail.com>

---
+++ 08-react-week5-assignment-1/jest.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  setupFilesAfterEnv: [
+    'jest-plugin-context/setup',
+    './jest.setup',
+    'given2/setup',
+  ],
+  coverageThreshold: {
+    global: {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100,
+    },
+  },
+};
